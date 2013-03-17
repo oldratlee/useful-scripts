9039f376a6f01e0ee8b3f9df71082a0b38e031ab
Jerry Lee <oldratlee@gmail.com>
2013-03-17 16:41:59 +0800

    #3 add dir option for find-in-jars.sh

--- find-in-jars.sh
+++ find-in-jars.sh
@@ -9,9 +9,44 @@
 #
 # @author Jerry Lee
 
-[ -z "$1" ] && { echo No find file pattern! ; exit 1; }  
+usage() {
+    cat <<EOF
+Usage: $0 [OPTION]... PATTERN
+Find file in the jar files under specified directory(recursive, include subdirectory)
+Example: $0 -d libs 'log4j\.properties$'
 
-find -iname '*.jar' | while read jarFile
+Options:
+    -d, --dir       the directory that find jar files
+    -h, --help      display this help and exit
+EOF
+    exit $1
+}
+
+ARGS=`getopt -a -o d:h -l dir:,help -- "$@"`
+[ $? -ne 0 ] && usage 1
+eval set -- "${ARGS}"
+
+while true; do
+    case "$1" in
+    -d|--dir)
+        dir="$2"
+        shift
+        ;;
+    -h|--help)
+        usage
+        ;;
+    --)
+        shift
+        break
+        ;;
+    esac
+    shift
+done
+
+
+[ -z ${dir} ] && dir=.
+
+find ${dir} -iname '*.jar' | while read jarFile
 do
         jar tf ${jarFile} | egrep "$1" | while read file
         do

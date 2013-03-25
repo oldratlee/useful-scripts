7a119d7c224e031d70da22b35da58727e1dbe069
Jerry Lee <oldratlee@gmail.com>
2013-03-25 19:45:46 +0800

    #10 add pid option

--- show-busy-java-threads.sh
+++ show-busy-java-threads.sh
@@ -25,37 +25,15 @@ Find out the highest cpu consumed threads of java, and print the stack of these 
 Example: ${PROG} -c 10
 
 Options:
+    -p, --pid       print thread stack from the specifed java process, default from all java process.
     -c, --count     set the thread count to show, default is 5
     -h, --help      display this help and exit
 EOF
     exit $1
 }
 
-ARGS=`getopt -a -o c:h -l count:,help -- "$@"`
-[ $? -ne 0 ] && usage 1
-eval set -- "${ARGS}"
-
-while true; do
-    case "$1" in
-    -c|--count)
-        count="$2"
-        shift 2
-        ;;
-    -h|--help)
-        usage
-        ;;
-    --)
-        shift
-        break
-        ;;
-    esac
-done
-[ -z "${count}" ] && count=5
-
-uuid=`date +%s`_${RANDOM}_$$
-
-ps -Leo pid,lwp,user,comm,pcpu --no-headers | awk '$4=="java"{print $0}' |
-sort -k5 -r -n | head --lines "${count}" | while read threadLine ; do
+printStackOfThreads() {
+    while read threadLine ; do
         pid=`echo ${threadLine} | awk '{print $1}'`
         threadId=`echo ${threadLine} | awk '{print $2}'`
         threadId0x=`printf %x ${threadId}`
@@ -74,6 +52,42 @@ sort -k5 -r -n | head --lines "${count}" | while read threadLine ; do
         
         redEcho "The stack of busy(${pcpu}%) thread(${threadId}/0x${threadId0x}) of java process(${pid}) of user(${user}):"
         sed "/nid=0x${threadId0x}/,/^$/p" -n ${jstackFile}
+    done
+}
+
+ARGS=`getopt -a -o c:p:h -l count:,pid:,help -- "$@"`
+[ $? -ne 0 ] && usage 1
+eval set -- "${ARGS}"
+
+while true; do
+    case "$1" in
+    -c|--count)
+        count="$2"
+        shift 2
+        ;;
+    -p|--pid)
+        pid="$2"
+        shift 2
+        ;;
+    -h|--help)
+        usage
+        ;;
+    --)
+        shift
+        break
+        ;;
+    esac
 done
+[ -z "${count}" ] && count=5
+
+uuid=`date +%s`_${RANDOM}_$$
+
+if [ -z ${pid} ] ; then 
+    ps -Leo pid,lwp,user,comm,pcpu --no-headers | awk '$4=="java"{print $0}' |
+    sort -k5 -r -n | head --lines "${count}" | printStackOfThreads
+else
+    ps -Leo pid,lwp,user,comm,pcpu --no-headers | awk '$1=="'${pid}'",$4=="java"{print $0}' |
+    sort -k5 -r -n | head --lines "${count}" | printStackOfThreads
+fi
 
 rm /tmp/${uuid}_*

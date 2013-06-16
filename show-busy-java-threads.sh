ce9f0907d7dc7250f93dd2c79488dcb54be71b22
oldratlee <oldratlee@gmail.com>
2013-06-16 14:48:17 +0800

    add trap exit

--- show-busy-java-threads.sh
+++ show-busy-java-threads.sh
@@ -9,15 +9,6 @@
 
 PROG=`basename $0`
 
-redEcho() {
-    if [ -c /dev/stdout ] ; then
-        # if stdout is console, turn on color output.
-        echo -e "\033[1;31m$@\033[0m"
-    else
-        echo "$@"
-    fi
-}
-
 usage() {
     cat <<EOF
 Usage: ${PROG} [OPTION]...
@@ -31,29 +22,6 @@ Options:
     -h, --help      display this help and exit
 EOF
     exit $1
-}
-
-printStackOfThreads() {
-    while read threadLine ; do
-        pid=`echo ${threadLine} | awk '{print $1}'`
-        threadId=`echo ${threadLine} | awk '{print $2}'`
-        threadId0x=`printf %x ${threadId}`
-        user=`echo ${threadLine} | awk '{print $3}'`
-        pcpu=`echo ${threadLine} | awk '{print $5}'`
-        
-        jstackFile=/tmp/${uuid}_${pid}
-        
-        [ ! -f "${jstackFile}" ] && {
-            jstack ${pid} > ${jstackFile} || {
-                redEcho "Fail to jstack java process ${pid}"
-                rm ${jstackFile}
-                continue
-            }
-        }
-        
-        redEcho "The stack of busy(${pcpu}%) thread(${threadId}/0x${threadId0x}) of java process(${pid}) of user(${user}):"
-        sed "/nid=0x${threadId0x}/,/^$/p" -n ${jstackFile}
-    done
 }
 
 ARGS=`getopt -a -o c:p:h -l count:,pid:,help -- "$@"`
@@ -81,8 +49,16 @@ while true; do
 done
 count=${count:-5}
 
-uuid=`date +%s`_${RANDOM}_$$
+redEcho() {
+    if [ -c /dev/stdout ] ; then
+        # if stdout is console, turn on color output.
+        echo -e "\033[1;31m$@\033[0m"
+    else
+        echo "$@"
+    fi
+}
 
+## Check the existence of jstack command!
 if ! which jstack &> /dev/null; then
     if [ -n "$JAVA_HOME" ] && [ -x "$JAVA_HOME/bin/jstack" ]; then
         export PATH=$PATH:$JAVA_HOME/bin
@@ -92,6 +68,36 @@ if ! which jstack &> /dev/null; then
     fi
 fi
 
+uuid=`date +%s`_${RANDOM}_$$
+
+cleanupWhenExit() {
+    rm /tmp/${uuid}_*
+}
+trap "cleanupWhenExit" EXIT
+
+printStackOfThreads() {
+    while read threadLine ; do
+        pid=`echo ${threadLine} | awk '{print $1}'`
+        threadId=`echo ${threadLine} | awk '{print $2}'`
+        threadId0x=`printf %x ${threadId}`
+        user=`echo ${threadLine} | awk '{print $3}'`
+        pcpu=`echo ${threadLine} | awk '{print $5}'`
+        
+        jstackFile=/tmp/${uuid}_${pid}
+        
+        [ ! -f "${jstackFile}" ] && {
+            jstack ${pid} > ${jstackFile} || {
+                redEcho "Fail to jstack java process ${pid}"
+                rm ${jstackFile}
+                continue
+            }
+        }
+        
+        redEcho "The stack of busy(${pcpu}%) thread(${threadId}/0x${threadId0x}) of java process(${pid}) of user(${user}):"
+        sed "/nid=0x${threadId0x}/,/^$/p" -n ${jstackFile}
+    done
+}
+
 if [ -z ${pid} ] ; then 
     ps -Leo pid,lwp,user,comm,pcpu --no-headers | awk '$4=="java"{print $0}' |
     sort -k5 -r -n | head --lines "${count}" | printStackOfThreads
@@ -99,6 +105,3 @@ else
     ps -Leo pid,lwp,user,comm,pcpu --no-headers | awk -v "pid=${pid}" '$1==pid,$4=="java"{print $0}' |
     sort -k5 -r -n | head --lines "${count}" | printStackOfThreads
 fi
-
-rm /tmp/${uuid}_*
-

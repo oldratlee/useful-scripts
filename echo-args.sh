7f3f2ea4ad37718b50329969bcdb202987eefe13
oldratlee <oldratlee@gmail.com>
2013-06-16 15:19:35 +0800

    improve redEcho of echo-args.sh

--- echo-args.sh
+++ echo-args.sh
@@ -1,8 +1,32 @@
 #!/bin/bash
 
-echo "(0/$#): [$0]"
+redEcho() {
+    if [ -c /dev/stdout ] ; then
+        # if stdout is console, turn on color output.
+        echo -ne "\033[1;31m"
+        echo -n "$@"
+        echo -ne "\033[0m"
+    else
+        echo -n "$@"
+    fi
+}
+
+echoArg() {
+    index=$1
+    count=$2
+    value=$3
+
+    echo -n "($index/$count): "
+    redEcho "["
+    echo -n "$value"
+    redEcho "]"
+    echo
+}
+
+
+echoArg 0 $# "$0"
 idx=1
 for a ; do
-    echo "($idx/$#): [$a]"
+    echoArg $idx $# "$a"
     idx=$((idx + 1))
 done

ae41c18caf025fe61dad759b6236810ff2bd0fd1
oldratlee <oldratlee@gmail.com>
2013-07-27 20:11:52 +0800

    add test case for parse.sh

--- parse.sh
+++ parse.sh
@@ -360,5 +360,7 @@ parseOpts() {
 # Main Methods
 #################################################
 
-parseOpts "a,a-long|b,b-long:|c,c-long+|d,d-long+" aa -a -x -b bb -c c.sh -a -b -c cc a1 a2 \; bb -d d.sh d1 d2 d3 \; bb2 -- cc dd
+parseOpts "a,a-long|b,b-long:|c,c-long+|d,d-long+" aa -a -b bb -c c.sh -a -b -c cc a1 a2 \; bb -d d.sh d1 d2 d3 \; cc -- dd ee
 showOptValueInfoList
+
+parseOpts "a,a-long|b,b-long:|c,c-long+|d,d-long+" aa -a -b bb -x -c c.sh -p pv -q qv \; bb -d -d d.sh d1 d2 d3 \; cc -- dd ee

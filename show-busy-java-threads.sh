1f7569fe2a802a8f79e46788478ad7940d471f24
Jerry Lee <oldratlee@gmail.com>
2013-12-22 13:40:48 +0800

    update show-busy-java-threads.sh

--- show-busy-java-threads.sh
+++ show-busy-java-threads.sh
@@ -25,7 +25,6 @@ EOF
 }
 
 ARGS=`getopt -n "$PROG" -a -o c:p:h -l count:,pid:,help -- "$@"`
-echo "$ARGS"
 [ $? -ne 0 ] && usage 1
 eval set -- "${ARGS}"
 
@@ -51,24 +50,22 @@ done
 count=${count:-5}
 
 redEcho() {
-    if [ -c /dev/stdout ] ; then
+    [ -c /dev/stdout ] && {
         # if stdout is console, turn on color output.
         echo -ne "\033[1;31m"
         echo -n "$@"
         echo -e "\033[0m"
-    else
-        echo "$@"
-    fi
+    } || echo "$@"
 }
 
 ## Check the existence of jstack command!
 if ! which jstack &> /dev/null; then
-    if [ -n "$JAVA_HOME" ] && [ -f "$JAVA_HOME/bin/jstack" ] && [ -x "$JAVA_HOME/bin/jstack" ]; then
-        export PATH=$PATH:$JAVA_HOME/bin
-    else
+    [ -n "$JAVA_HOME" ] && [ -f "$JAVA_HOME/bin/jstack" ] && [ -x "$JAVA_HOME/bin/jstack" ] && {
+        export PATH="$JAVA_HOME/bin:$PATH"
+    } || {
         redEcho "Error: jstack not found on PATH and JAVA_HOME!"
         exit 1
-    fi
+    }
 fi
 
 uuid=`date +%s`_${RANDOM}_$$
@@ -78,7 +75,7 @@ cleanupWhenExit() {
 }
 trap "cleanupWhenExit" EXIT
 
-printStackOfThreads() {
+printStackOfThread() {
     while read threadLine ; do
         pid=`echo ${threadLine} | awk '{print $1}'`
         threadId=`echo ${threadLine} | awk '{print $2}'`
@@ -101,10 +98,10 @@ printStackOfThreads() {
     done
 }
 
-if [ -z "${pid}" ] ; then 
+[ -z "${pid}" ] && {
     ps -Leo pid,lwp,user,comm,pcpu --no-headers | awk '$4=="java"{print $0}' |
-    sort -k5 -r -n | head --lines "${count}" | printStackOfThreads
-else
+    sort -k5 -r -n | head --lines "${count}" | printStackOfThread
+} || {
     ps -Leo pid,lwp,user,comm,pcpu --no-headers | awk -v "pid=${pid}" '$1==pid,$4=="java"{print $0}' |
-    sort -k5 -r -n | head --lines "${count}" | printStackOfThreads
-fi
+    sort -k5 -r -n | head --lines "${count}" | printStackOfThread
+}

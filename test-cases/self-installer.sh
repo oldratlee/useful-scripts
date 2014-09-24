7d3f4a53f1fa4681d912b5b9444ea16149ae22eb
Jerry Lee <oldratlee@gmail.com>
2014-09-24 22:55:59 +0800

    update self-installer.sh #32

--- test-cases/self-installer.sh
+++ test-cases/self-installer.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 if which svn &> /dev/null; then
+	[ ! -d "/tmp/useful-shells-$USER" ] &&
 	svn checkout https://github.com/oldratlee/useful-shells/trunk "/tmp/useful-shells-$USER"
 fi
+
 export PATH="$PATH:/tmp/useful-shells-$USER"

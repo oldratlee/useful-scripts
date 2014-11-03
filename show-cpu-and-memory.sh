c39bc079769efafe937c90b5fd03fe06aa3a3d19
飒然Hang <superhj1987@126.com>
2014-11-03 17:55:17 +0800

    refactor show-cpu-and-memory.sh: fix check pid

--- show-cpu-and-memory.sh
+++ show-cpu-and-memory.sh
@@ -9,6 +9,11 @@
 
 for i in `ps -ef | egrep -v "awk|$0" | awk '/'$1'/{print $2}'` 
 do 
+	# not pid
+	if [[ $i == *[!0-9]* ]]; then
+		continue
+	fi
+
     mem=`cat /proc/$i/status 2> /dev/null | grep VmRSS | awk '{print $2" " $3}'` 
     cpu=`top -n 1 -b | awk '$1=='$i'{print $9}'` 
 

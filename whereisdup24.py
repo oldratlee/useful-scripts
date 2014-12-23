369f7cad6bac40633679d0ad7b375b0c6581f2e5
Jerry Lee <oldratlee@gmail.com>
2014-12-23 16:50:33 +0800

    reformat code

--- whereisdup24.py
+++ whereisdup24.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'tg123'
 
 from zipfile import ZipFile
 from glob import glob
 from itertools import groupby
 from optparse import OptionParser
 from fnmatch import fnmatchcase
+
 
 class Printer:
     def before(self):
@@ -16,6 +20,7 @@ class Printer:
     def after(self):
         pass
 
+
 class JarOnlyPrinter(Printer):
     bucket = []
 
@@ -24,14 +29,17 @@ class JarOnlyPrinter(Printer):
             print jars
             self.bucket.append(jars)
 
+
 class AllPrinter(Printer):
     def put(self, clzname, jars):
-        print clzname,jars
+        print clzname, jars
 
-walkzip = lambda f  : [(cf,f) for cf in ZipFile(f).namelist() if cf.lower().endswith('.class')]
+
+walkzip = lambda f: [(cf, f) for cf in ZipFile(f).namelist() if cf.lower().endswith('.class')]
+
 
 def matchexclude(z, excludes):
-    if isinstance(excludes,str):
+    if isinstance(excludes, str):
         excludes = excludes.split(',')
 
     for patten in excludes:
@@ -40,21 +48,23 @@ def matchexclude(z, excludes):
 
     return False
 
+
 def zfilter(clzname, jars):
     jars = tuple(jars)
     if len(jars) > 1:
-        yield (clzname,[j[1] for j in jars ])
+        yield (clzname, [j[1] for j in jars])
+
 
 if __name__ == '__main__':
     op = OptionParser('usage: %prog [options] dir1 dir2 ...')
 
-    op.add_option("-r", dest="recursive", default=False, action="store_true") # not impl
+    op.add_option("-r", dest="recursive", default=False, action="store_true")  # not impl
     op.add_option("-v", dest="verbose", default=False, action="store_true")
     op.add_option("-e", "--excludes", dest="excludes", default=[])
 
-    options,args = op.parse_args()
-    if len(args) == 0 :
-        args = [ '.' ]
+    options, args = op.parse_args()
+    if len(args) == 0:
+        args = ['.']
 
     if options.verbose:
         printer = AllPrinter()
@@ -62,8 +72,10 @@ if __name__ == '__main__':
         printer = JarOnlyPrinter()
 
     printer.before()
-    for t in groupby(sorted([c for p in args for z in glob(p + '/*.jar') if not matchexclude(z, options.excludes) for c in walkzip(z)]),lambda x : x[0]):
+    for t in groupby(sorted(
+            [c for p in args for z in glob(p + '/*.jar') if not matchexclude(z, options.excludes) for c in walkzip(z)]),
+                     lambda x: x[0]):
         for clzname, jars in zfilter(*t):
-            printer.put(clzname,jars)
+            printer.put(clzname, jars)
 
     printer.after()

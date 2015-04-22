6fe77dbded33bc0e085abe63b8494b8be6c55cd2
Bryant Hang <superhj1987@126.com>
2015-04-22 11:58:31 +0800

    Update monitor-host.sh
    fix bug

--- monitor-host.sh
+++ monitor-host.sh
@@ -10,10 +10,10 @@
 DELAY=10
 COUNT=60
 
-LOG_PATH='monitor_path'
+LOG_PATH='monitor_logs'
 
-if ! [ -d $monitor_log_path ]; then
-        mkdir -p $monitor_log_path
+if ! [ -d $LOG_PATH ]; then
+        mkdir -p $LOG_PATH
 fi
 
 cur_date="`date +%Y%m%d`"
@@ -25,16 +25,16 @@ io_log_path=$LOG_PATH'/io_'$cur_date'.log'
 network_log_path=$LOG_PATH'/network_'$cur_date'.log'
 
 # total performance check
-top -b -d $DELAY -n $COUNT > top_log_path 2>&1 &
+top -b -d $DELAY -n $COUNT > $top_log_path 2>&1 &
 
 # memory check
-vmstat $DELAY $COUNT > memory_log_path 2>&1 &
+vmstat $DELAY $COUNT > $memory_log_path 2>&1 &
 
 # cpu check
-sar -u $DELAY $COUNT > cpu_log_path 2>&1 &
+sar -u $DELAY $COUNT > $cpu_log_path 2>&1 &
 
 # IO check
-iostat $DELAY $COUNT > io_log_path 2>&1 &
+iostat $DELAY $COUNT > $io_log_path 2>&1 &
 
 # network check
-sar -n DEV $DELAY $COUNT > network_log_path 2>&1 &
+sar -n DEV $DELAY $COUNT > $network_log_path 2>&1 &

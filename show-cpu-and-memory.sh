30404bb0b7fcc91a4c2497330f81cce310be6dd1
Jerry Lee <oldratlee@gmail.com>
2015-04-23 17:59:32 +0800

    clean show-cpu-and-memory.sh

--- show-cpu-and-memory.sh
+++ show-cpu-and-memory.sh
@@ -6,29 +6,20 @@
 #   $ ./show-cpu-and-memory.sh
 #
 # @author Bryant Hang
-cur_date="`date +%Y%m%d`"
 
-total_mem="`free -m | grep 'Mem'`"
-total_cpu="`top -n 1 | grep 'Cpu'`"
+readonly cur_date="`date +%Y%m%d`"
+
+readonly total_mem="`free -m | grep 'Mem'`"
+readonly total_cpu="`top -n 1 | grep 'Cpu'`"
 
 echo '**********'$cur_date'**********'
+echo
+echo "total memory: $total_mem total cpu: $total_cpu"
+echo
 
-echo ''
+for pid in `ps -ef | awk 'NR > 0 {print $2}'` ; do
+    mem=`cat /proc/$pid/status 2> /dev/null | grep VmRSS | awk '{print $2 $3}'`
+    cpu=`top -n 1 -b | awk -v "pid=${pid}" '$1==pid {print $9}'`
 
-echo $total_mem
-echo $total_cpu
-
-echo ''
-
-for i in `ps -ef | grep -v "awk|$0" | awk '{print $2}'` 
-do 
-	# not pid
-	if [[ $i == *[!0-9]* ]]; then
-		continue
-	fi
-
-    mem=`cat /proc/$i/status 2> /dev/null | grep VmRSS | awk '{print $2" " $3}'` 
-    cpu=`top -n 1 -b | awk '$1=='$i'{print $9}'` 
-
-    echo 'pid: '$i', memory: '$mem', cpu:'$cpu'%'
+    echo "pid: $pid, memory: $mem, cpu:$cpu%"
 done

94ed26056e9dd3ef3d85458b633912ea983b13d6
romechax <47410463+romechax@users.noreply.github.com>
2019-06-25 15:51:16 +0530

    test

--- test-cases/self-installer.sh
+++ test-cases/self-installer.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-
+echo 'this is test edit'
 if which svn &> /dev/null; then
 	[ ! -d "/tmp/useful-scripts-$USER" ] &&
 	svn checkout https://github.com/oldratlee/useful-scripts/trunk "/tmp/useful-scripts-$USER"
 fi
 
 export PATH="$PATH:/tmp/useful-scripts-$USER"
+

69154527c0757f48d262c5de8bfde680d763e63f
romechax <47410463+romechax@users.noreply.github.com>
2019-06-25 15:56:35 +0530

    Update self-installer.sh

--- test-cases/self-installer.sh
+++ test-cases/self-installer.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo 'this is test edit'
+echo 'this is test edit#2'
 if which svn &> /dev/null; then
 	[ ! -d "/tmp/useful-scripts-$USER" ] &&
 	svn checkout https://github.com/oldratlee/useful-scripts/trunk "/tmp/useful-scripts-$USER"

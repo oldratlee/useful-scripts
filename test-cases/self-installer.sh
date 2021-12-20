26df16f5d4ea1eda28ec464d57c5fffaff942b21
removedporn <86824510+removedporn@users.noreply.github.com>
2021-12-20 19:36:22 +0800

    Delete test-cases directory

--- test-cases/self-installer.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if command -v svn &> /dev/null; then
-	[ ! -d "/tmp/useful-scripts-$USER" ] &&
-	svn checkout https://github.com/oldratlee/useful-scripts/branches/release-2.x "/tmp/useful-scripts-$USER"
-fi
-
-export PATH="$PATH:/tmp/useful-scripts-$USER/bin:/tmp/useful-scripts-$USER/legacy-bin"

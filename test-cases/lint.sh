0001765dc6bd16dd7c45a9bd00cddf65859bf483
Jerry Lee <oldratlee@gmail.com>
2023-09-06 22:51:30 +0800

    build(CI): add `lint.yaml`

---
+++ test-cases/lint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -eEuo pipefail
+
+# cd to the root of the project
+cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"/..
+
+find bin lib legacy-bin -type f |
+  grep -Pv '/show-duplicate-java-classes$' |
+  grep -Pv '/\.editorconfig$' |
+  xargs --verbose shellcheck --shell=bash
